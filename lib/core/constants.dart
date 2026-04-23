eee8dd535b578ab33b76a9a9bb33ff0c4d42ec59
Edwin Joy <161407507+edwwwi@users.noreply.github.com>
2026-04-23 23:31:05 +0530

    update4

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -8,7 +8,7 @@ class AppColors {
   static const Color textDark = Color(0xFF2D3436);
   static const Color textLight = Color(0xFF636E72);
   static const Color error = Color(0xFFe74c3c);
-
+////////////////
   // Categories
   static const Color food = Color(0xFFFF7675);
   static const Color petrol = Color(0xFF74B9FF);
